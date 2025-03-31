a7c55d62e0a59aa61c116c423d0b97f4979f25d1
estebanrp <estebanmilkes@gmail.com>
2025-03-31 00:36:31 -0300

    funtional flashcard of example

--- controllers/cDeck.js
+++ controllers/cDeck.js
@@ -4,34 +4,53 @@ import error from "../middlewares/error.js";
 const cDeck = {
     getDeck: async (req, res) => {
         try {
-            let id = parseInt(req.params.id);
+            const id = parseInt(req.params.id);
             const deck = await mDeck.getDeck(id);
             const title = await mDeck.getDeckTitle(id);
+
+            let currentIndex = req.query.index ? parseInt(req.query.index) : 0;
 
             if (!deck) {
                 return error.e404(req, res);
             }
 
-            let currentIndex = req.query.index ? parseInt(req.query.index) : 0;
-
-            const renderCard = (title) => {
+            const renderCard = () => {
                 const card = deck[currentIndex];
-                res.render('deck', { currentIndex, id, title, card, hasNext: currentIndex < deck.length - 1, hasPrev: currentIndex > 0 });
+                const deckLength = deck.length; 
+                res.render('deck', { deckLength, currentIndex, id, title, card, hasNext: currentIndex < deck.length - 1, hasPrev: currentIndex > 0 });
             };
 
-            renderCard(title);
-
-            req.on('nextCard', () => {
-                if (currentIndex < deck.length - 1) {
-                    currentIndex++;
-                    renderCard(title);
-                }
-            });
+            renderCard();
 
         } catch (err) {
             error.e500(req, res, err);
         }
+    },
+    nextCard: (req, res) => {
+        let currentIndex = parseInt(req.query.index) || 0;
+        let hasNext = req.query.hasNext;
+        if (hasNext == "true") {
+            currentIndex++;
+            res.redirect(`/deck/${req.params.id}?index=${currentIndex}`);
+        } else {
+            currentIndex = 0;
+            res.redirect(`/deck/${req.params.id}?index=${currentIndex}`);
+        }
+
+    },
+    prevCard: (req, res) => {
+        let currentIndex = parseInt(req.query.index) || 0;
+        let hasPrev = req.query.hasPrev;
+        let deckLength = req.query.long;
+        if (hasPrev == 'true') {
+            currentIndex--;
+        } else {
+            currentIndex = deckLength - 1;
+        }
+        res.redirect(`/deck/${req.params.id}?index=${currentIndex}`);
     }
 };
+
+
 
 export default cDeck;
--- routes/rDeck.js
+++ routes/rDeck.js
@@ -4,5 +4,7 @@ import cDeck from "../controllers/cDeck.js";
 const routes = Router();
 
 routes.get('/deck/:id', cDeck.getDeck);
+routes.get('/deck/:id/next', cDeck.nextCard);
+routes.get('/deck/:id/prev', cDeck.prevCard);
 
 export default routes;
